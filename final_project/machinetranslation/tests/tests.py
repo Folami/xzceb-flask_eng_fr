d105ce382408114e87bdfe8bc30e12af49b33533
Folami <folami.bidmus@gmail.com>
2023-01-20 16:28:08 +0100

    Update tests.py

--- final_project/machinetranslation/tests/tests.py
+++ final_project/machinetranslation/tests/tests.py
@@ -6,13 +6,13 @@ class TestEnglishToFrench(unittest.TestCase):
     def test1(self): 
         self.assertEqual(english_to_french(None), None) # test when null is given as input the output is null.
         self.assertEqual(english_to_french('Hello'), 'Bonjour')  # test when ‘Hello’ is given as input the output is ‘Bonjour’.
-        self.assertEqual(english_to_french('Mr'), 'Monsieur')  # test when ‘Mr’ is given as input the output is ‘Monsieur’.
+        self.assertNotEqual(english_to_french('Miss'), 'Monsieur')  # test when ‘Mr’ is given as input the output is ‘Monsieur’.
         
 
 class TestFrenchToEnglish(unittest.TestCase): 
     def test1(self): 
         self.assertEqual(french_to_english(None), None) # test when null is given as input the output is null.
         self.assertEqual(french_to_english('Bonjour'), 'Hello') # test when ‘Bonjour’ is given as input the output is ‘Hello’.
-        self.assertEqual(french_to_english('Mademoiselle'), 'Miss') # test when ‘Mademoiselle’ is given as input the output is ‘Miss’.
+        self.assertNotEqual(french_to_english('Mademoiselle'), 'Mr') # test when ‘Mademoiselle’ is given as input the output is ‘Miss’.
         
 unittest.main()
